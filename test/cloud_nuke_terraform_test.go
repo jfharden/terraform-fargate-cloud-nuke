46644901f947f9e50e057c925217dc31c6380643
Jonathan Harden <jonathan.harden@mydrivesolutions.com>
2020-04-13 23:20:43 +0100

    Added extra tests on the ecr repo

--- test/cloud_nuke_terraform_test.go
+++ test/cloud_nuke_terraform_test.go
@@ -5,6 +5,10 @@ import (
 	"strings"
 	"testing"
 
+	awsSdk "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecr"
+
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -13,9 +17,19 @@ import (
 )
 
 func TestFargateCloudNuke(t *testing.T) {
-	randomName := fmt.Sprintf("cloud-nuke-test-%s", strings.ToLower(random.UniqueId()))
-
 	awsRegion := "eu-west-1"
+
+	awsSession, err := session.NewSession(
+		&awsSdk.Config{
+			Region: awsSdk.String(awsRegion),
+		},
+	)
+
+	if err != nil {
+		t.Errorf("Couldn't create AWS session %s", err)
+	}
+
+	randomName := fmt.Sprintf("cloud-nuke-test-%s", strings.ToLower(random.UniqueId()))
 
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/terraform-fargate-cloud-nuke-basic/",
@@ -33,13 +47,32 @@ func TestFargateCloudNuke(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 
-	ValidateECR(t, terraformOptions, randomName)
+	ValidateECR(t, terraformOptions, randomName, awsSession)
 }
 
-func ValidateECR(t *testing.T, terraformOptions *terraform.Options, randomName string) {
+func ValidateECR(t *testing.T, terraformOptions *terraform.Options, randomName string, awsSession *session.Session) {
 	accountId := aws.GetAccountId(t)
 	expectedEcrArn := fmt.Sprintf("arn:aws:ecr:eu-west-1:%s:repository/%s", accountId, randomName)
 	ecrArn := terraform.Output(t, terraformOptions, "ecs_repository_arn")
 
 	assert.Equal(t, expectedEcrArn, ecrArn)
+
+	ecrService := ecr.New(awsSession)
+	result, err := ecrService.DescribeRepositories(
+		&ecr.DescribeRepositoriesInput{
+			RepositoryNames: []*string{
+				awsSdk.String(randomName),
+			},
+		},
+	)
+
+	if err != nil {
+		t.Errorf("Error trying to describe ECR repository %s. Error was %s", randomName, err)
+	}
+
+	assert.Equal(t, len(result.Repositories), 1, "Exactly 1 ECR repository")
+
+	repository := result.Repositories[0]
+
+	assert.True(t, awsSdk.BoolValue(repository.ImageScanningConfiguration.ScanOnPush), "Image scanning enabled on ECR repo")
 }
--- test/go.mod
+++ test/go.mod
@@ -3,6 +3,7 @@ module github.com/jfharden/terraform-fargate-cloud-nuke
 go 1.13
 
 require (
+	github.com/aws/aws-sdk-go v1.27.1
 	github.com/gruntwork-io/terratest v0.26.3
 	github.com/stretchr/testify v1.5.1
 )
