6ea82522c0fb917b63fda4b4e27b522c26908528
Jonathan Harden <jfharden@gmail.com>
2020-04-13 17:20:45 +0100

    Add initial docker tests

---
+++ test/cloud_nuke_dockerfile_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/docker"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDockerfile(t *testing.T) {
+	t.Parallel()
+
+	tag := "jfharden/fargate-cloud-nuke:dockerfile"
+
+	buildOptions := &docker.BuildOptions{
+		Tags: []string{tag},
+	}
+
+	docker.Build(t, "../", buildOptions)
+
+	output := docker.Run(t, tag, &docker.RunOptions{})
+	assert.Equal(t, "cloud-nuke version v0.1.17", output)
+}
+
+func TestDockerfileOverrideVersion(t *testing.T) {
+	t.Parallel()
+
+	tag := "jfharden/fargate-cloud-nuke:dockerfile-override-version"
+
+	buildOptions := &docker.BuildOptions{
+		Tags: []string{tag},
+
+		BuildArgs: []string{
+			"CLOUD_NUKE_VERSION=v0.1.16",
+		},
+	}
+
+	docker.Build(t, "../", buildOptions)
+
+	output := docker.Run(t, tag, &docker.RunOptions{})
+	assert.Equal(t, "cloud-nuke version v0.1.16", output)
+}
+
+func TestDockerfileOverrideBinary(t *testing.T) {
+	t.Parallel()
+
+	tag := "jfharden/fargate-cloud-nuke:dockerfile-override-binary"
+
+	buildOptions := &docker.BuildOptions{
+		Tags: []string{tag},
+
+		BuildArgs: []string{
+			"CLOUD_NUKE_BINARY=cloud-nuke_linux_386",
+		},
+	}
+
+	docker.Build(t, "../", buildOptions)
+
+	runOptions := &docker.RunOptions{
+		Command: []string{
+			"ls", "/cloud-nuke/cloud-nuke_linux_386",
+		},
+	}
+
+	output := docker.Run(t, tag, runOptions)
+	assert.Equal(t, "/cloud-nuke/cloud-nuke_linux_386", output)
+
+	runOptions.Command = []string{
+		"readlink", "-f", "/cloud-nuke/cloud-nuke",
+	}
+
+	output = docker.Run(t, tag, runOptions)
+	assert.Equal(t, "/cloud-nuke/cloud-nuke_linux_386", output)
+}
---
+++ test/go.mod
@@ -0,0 +1,8 @@
+module github.com/jfharden/terraform-fargate-cloud-nuke
+
+go 1.13
+
+require (
+	github.com/gruntwork-io/terratest v0.26.3
+	github.com/stretchr/testify v1.5.1
+)
